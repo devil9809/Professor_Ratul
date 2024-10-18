6ba645f760d10ff3d6b5dd13a30df54ad829df25
Mr Professor <79388564+devil9809@users.noreply.github.com>
2024-10-18 16:41:15 +0600

    installerx.sh

--- installerx.sh
+++ installerx.sh
@@ -5,6 +5,6 @@ pkg install -y git tsu python wpa-supplicant pixiewps iw
 
 git clone --depth 1 https://github.com/devil9809/Ratul Ratul
 
-chmod +x Ratul/ratul.py
+chmod +x Ratul/ratulx.py
 
-printf "###############################################\n#  All done! Now you can run Professor Ratul with\n#   sudo python Ratul/ratul.py -i wlan0 -K\n#\n#  To update, run\n#   (cd Ratul && git pull)\n###############################################\n"
+printf "###############################################\n#  All done! Now you can run Professor Ratul with\n#   sudo python Ratul/ratulx.py -i wlan0  --iface-down -K\n#\n#  To update, run\n#   (cd Ratul && git pull)\n###############################################\n"
